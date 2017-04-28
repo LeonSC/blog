e05a1a925b3eb74009adfa8d0b664f9f84e81f63
LeonSC <sasgsc@gmaill.com>
2017-04-28 23:37:46 +0800

    hotmai邮件发送

--- src/blog/component/MailComponent.java
+++ src/blog/component/MailComponent.java
@@ -9,7 +9,7 @@ import blog.startup.Config;
 @Component
 public class MailComponent {
 
-	private SimpleEmail getSimpleEmail() throws EmailException
+	public SimpleEmail getSimpleEmail() throws EmailException
 	{
 		SimpleEmail email = new SimpleEmail();
 		email.setHostName(Config.esmtp); // 发送服务器
@@ -21,10 +21,23 @@ public class MailComponent {
 		return email;
 	}
 	
+	private SimpleEmail getHotmailSimpleEmail() throws EmailException
+	{
+		SimpleEmail client = new SimpleEmail();
+		client.setHostName("smtp-mail.outlook.com"); // 发送服务器
+		client.setSmtpPort(587); // 端口
+		client.setAuthentication(Config.efrom, Config.epw); // 发送邮件的用户名和密码
+		client.setStartTLSEnabled(true);
+		client.setStartTLSRequired(true);
+		client.setFrom(Config.efrom); // 发送邮箱
+		client.setCharset("UTF-8"); // 编码
+		return client;
+	}
+	
 	public int sendComfirmEmail(String email, String content)
 	{
 		try {
-			SimpleEmail client = this.getSimpleEmail();
+			SimpleEmail client = this.getHotmailSimpleEmail();
 			client.addTo(email);
 			client.setSubject("邮箱验证");// 主题
 			client.setMsg(content); // 内容
@@ -38,7 +51,7 @@ public class MailComponent {
 	public int sendDepositCardEmail(String email, String code)
 	{
 		try {
-			SimpleEmail client = this.getSimpleEmail();
+			SimpleEmail client = this.getHotmailSimpleEmail();
 			client.addTo(email);
 			client.setSubject("宅次元");// 主题
 			client.setMsg("请登录后在个人面板中兑换."+code); // 内容
@@ -47,5 +60,24 @@ public class MailComponent {
 			e.printStackTrace();
 		}
 		return 0;
+	}
+	
+	public static void main(String[] args) throws EmailException {
+		
+		SimpleEmail client = new SimpleEmail();
+		client.setHostName("smtp-mail.outlook.com"); // 发送服务器
+		client.setSmtpPort(587); // 端口
+		client.setAuthentication("otakulacom@hotmail.com", "akinaclub13832840620"); // 发送邮件的用户名和密码
+		client.setStartTLSEnabled(true);
+		client.setStartTLSRequired(true);
+		//client.setSSLOnConnect(true); // gmail需要设置SSL安全设置
+		client.setFrom("otakulacom@hotmail.com"); // 发送邮箱
+		
+		//client.set
+		client.setCharset("UTF-8"); // 编码
+		client.addTo("sasgsc@qq.com");
+		client.setSubject("邮箱验证");// 主题
+		client.setMsg("test"); // 内容
+		client.send();
 	}
 }
